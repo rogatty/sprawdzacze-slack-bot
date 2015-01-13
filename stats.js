1b9490987d137e446c7a35a4cdb5dbe8e3ddd249
Igor Rogatty <igor@wikia-inc.com>
2015-01-13 10:50:50 +0100

    Disable stats temporarily

--- stats.js
+++ stats.js
@@ -1,25 +1,32 @@
 'use strict';
 
-var //config = require('./config.json'),
-	db = require('./db');
+/*var config = require('./config.json'),
+	db = require('./db');*/
 
 module.exports = function (statsMatches, userId, res) {
-	if (typeof statsMatches[1] !== 'undefined') {
-		userId = statsMatches[1];
-	}
-
-	db.getNumberOfMatches(userId)
-		.then(function (numberOfMatches) {
-			var payload = {
-				text: 'Stats for ' + userId,
-				attachments: [{
-					fields: [{
-						title: 'Number of matches',
-						value: numberOfMatches,
-						short: true
-					}]
-				}]
-			};
-			res.status(200).json(payload);
-		});
+	var payload = {
+		text: 'Stats are currently disabled. They will be back after the author has time to make them less incriminating. Don\'t stop signing in!'
+	};
+	res.status(200).json(payload);
 };
+
+//module.exports = function (statsMatches, userId, res) {
+//	if (typeof statsMatches[1] !== 'undefined') {
+//		userId = statsMatches[1];
+//	}
+//
+//	db.getNumberOfMatches(userId)
+//		.then(function (numberOfMatches) {
+//			var payload = {
+//				text: 'Stats for ' + userId,
+//				attachments: [{
+//					fields: [{
+//						title: 'Number of matches',
+//						value: numberOfMatches,
+//						short: true
+//					}]
+//				}]
+//			};
+//			res.status(200).json(payload);
+//		});
+//};
