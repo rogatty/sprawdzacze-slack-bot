6e617e5f7d901d38c65aac072005c7a3bde812ac
Igor Rogatty <igor@wikia-inc.com>
2015-01-17 18:50:28 +0100

    Move files to lib; dashboard route; routes refactored

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 'use strict';
 
 var express = require('express'),
-	bodyParser = require('body-parser'),
-	bot = require('./bot'),
-	db = require('./db'),
-	config = require('./config.json'),
 	app = express(),
-	port = process.env.PORT || 3000;
+	bodyParser = require('body-parser'),
+	port = process.env.PORT || 3000,
+
+	bot = require('./lib/bot'),
+	dashboard = require('./lib/dashboard'),
+	db = require('./lib/db'),
+	config = require('./config.json');
 
 // body parser middleware
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-// test route
-app.get('/', function (req, res) {
-	res.status(200).send('<html><body><img src="' + config.slapGif + '"></body></html>');
-});
+// index
+app.route('/')
+	.get(function (req, res) {
+		res.status(200).send('<html><body><img src="' + config.slapGif + '"></body></html>');
+	});
 
 // error handler
 app.use(function (req, res, next, err) {
@@ -28,14 +31,17 @@ app.listen(port, function () {
 	console.log('Slack bot listening on port ' + port);
 });
 
-app.post('/bot', bot);
+app.route('/bot').post(bot);
 
-app.get('/setdb', function (req, res) {
-	db.save()
-		.then(function (msg) {
-			res.status(200).send(msg);
-		})
-		.catch(function (error) {
-			res.status(500).send(error);
-		});
-});
+app.route('/dashboard/:secret').get(dashboard);
+
+app.route('/setdb')
+	.get(function (req, res) {
+		db.setup()
+			.then(function (msg) {
+				res.status(200).send(msg);
+			})
+			.catch(function (error) {
+				res.status(500).send(error);
+			});
+	});
